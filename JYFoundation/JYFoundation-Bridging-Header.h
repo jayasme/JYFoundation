2043df7dd51a6d2b45cf9147837583fc556bfecc
jayasme <sunshine121981@126.com>
2018-11-13 15:18:20 +0800

    Optmize the project

--- JYFoundation/JYFoundation-Bridging-Header.h
+++ JYFoundation/JYFoundation-Bridging-Header.h
@@ -6,9 +6,4 @@
 //  Copyright © 2018年 jayasme All rights reserved.
 //
 
-#import "WXApi.h"
-#import "WXApiObject.h"
-#import "WeiboSDK.h"
-#import <RongIMLib/RongIMLib.h>
-
 #import <CommonCrypto/CommonHMAC.h>
