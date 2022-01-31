795f9052fa5ee383ae4bf900f0077e02f96e94c5
scott.rong <sunshine121981@hotmail.com>
2022-01-31 11:47:41 +0800

    extend date

--- JYFoundation/Extensions/Date+JY.swift
+++ JYFoundation/Extensions/Date+JY.swift
@@ -30,20 +30,140 @@ extension Date {
         return formatter.string(from: self)
     }
     
-    public func descriptionString() -> String {
-        let interval = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute], from: self, to: Date.now())
-        if let year = interval.year, year >= 1 {
-            return String(format: "%d年前", year)
-        } else if let month = interval.month, month >= 1 {
-            return String(format: "%d个月前", month)
-        } else if let day = interval.day, day >= 1 {
-            return String(format: "%d天前", day)
-        } else if let hour = interval.hour, hour >= 1 {
-            return String(format: "%d小时前", hour)
-        } else if let minute = interval.minute, minute >= 1 {
-            return String(format: "%d分钟前", minute)
+    // Properties
+    
+    public var year: Int {
+        let calendar = Calendar(identifier: .iso8601)
+        return calendar.component(.year, from: self)
+    }
+    
+    public var month: Int {
+        let calendar = Calendar(identifier: .iso8601)
+        return calendar.component(.month, from: self)
+    }
+    
+    public var monthLong: String {
+        let calendar = Calendar(identifier: .iso8601)
+        return calendar.monthSymbols[self.month]
+    }
+    
+    public var monthShort: String {
+        let calendar = Calendar(identifier: .iso8601)
+        return calendar.shortMonthSymbols[self.month]
+    }
+    
+    public var monthVeryShort: String {
+        let calendar = Calendar(identifier: .iso8601)
+        return calendar.veryShortStandaloneMonthSymbols[self.month]
+    }
+    
+    public var day: Int {
+        let calendar = Calendar(identifier: .iso8601)
+        return calendar.component(.day, from: self)
+    }
+    
+    public var weekday: Int {
+        let calendar = Calendar(identifier: .iso8601)
+        return calendar.component(.weekday, from: self)
+    }
+
+    public var weekdayLong: String {
+        let calendar = Calendar(identifier: .iso8601)
+        return calendar.standaloneWeekdaySymbols[self.weekday]
+    }
+    
+    public var weekdayShort: String {
+        let calendar = Calendar(identifier: .iso8601)
+        return calendar.shortWeekdaySymbols[self.weekday]
+    }
+    
+    public var weekdayVeryShort: String {
+        let calendar = Calendar(identifier: .iso8601)
+        return calendar.veryShortStandaloneWeekdaySymbols[self.weekday]
+    }
+    
+    public var hour12: Int {
+        let hour = self.hour24
+        if (hour > 12) {
+            return hour - 12
         }
-        
-        return "刚刚"
+        return hour
+    }
+    
+    public var hour12Period: String {
+        let calendar = Calendar(identifier: .iso8601)
+        let hour = self.hour24
+        if (hour > 12) {
+            return calendar.pmSymbol
+        }
+        return calendar.amSymbol
+    }
+    
+    public var hour24: Int {
+        let calendar = Calendar(identifier: .iso8601)
+        return calendar.component(.hour, from: self)
+    }
+    
+    public var minute: Int {
+        let calendar = Calendar(identifier: .iso8601)
+        return calendar.component(.minute, from: self)
+    }
+    
+    public var second: Int {
+        let calendar = Calendar(identifier: .iso8601)
+        return calendar.component(.second, from: self)
+    }
+    
+    // local calendars
+    
+    public var rocYear: Int {
+        let calendar = Calendar(identifier: .republicOfChina)
+        return calendar.component(.year, from: self)
+    }
+    
+    public var chineseMonth: Int {
+        let calendar = Calendar(identifier: .chinese)
+        return calendar.component(.year, from: self)
+    }
+    
+    public var chineseMonthLong: String {
+        let months = ["一月", "二月", "三月", "四月", "五月", "六月", "七月", "八月", "九月", "十月", "十一月", "十二月"]
+        return months[self.chineseMonth - 1]
+    }
+    
+    public var chineseMonthShort: String {
+        let months = ["一", "二", "三", "四", "五", "六", "七", "八", "九", "十", "十一", "十二"]
+        return months[self.chineseMonth - 1]
+    }
+    
+    public var chineseDay: Int {
+        let calendar = Calendar(identifier: .chinese)
+        return calendar.component(.day, from: self)
+    }
+    
+    public var chineseDayString: String {
+        let days = ["一", "二", "三", "四", "五", "六", "七", "八", "九", "十"]
+        let day = self.chineseDay
+        if (day < 10) {
+            return "初" + days[day - 1]
+        }
+        if (day < 20) {
+            return "十" + days[day % 10 - 1]
+        }
+        if (day == 20) {
+            return "二十"
+        }
+        if (day < 30) {
+            return "廿" + days[day % 10 - 1]
+        }
+        if (day == 30) {
+            return "三十"
+        }
+        return "三" + days[day % 10 - 1]
+    }
+    
+    public var japaneseYear: Int {
+        let calendar = Calendar(identifier: .japanese)
+        return calendar.component(.year, from: self)
     }
 }
