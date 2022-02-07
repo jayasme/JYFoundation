27a2bab5b9134052489478385a6f779a26c9bcf1
scott.rong <sunshine121981@hotmail.com>
2022-02-07 15:13:45 +0800

    fix: uiapplication.keywindow

--- JYFoundation/Extensions/UIApplication+JY.swift
+++ JYFoundation/Extensions/UIApplication+JY.swift
@@ -11,17 +11,13 @@ import UIKit
 
 extension UIApplication {
     
-    var keyWindow: UIWindow? {
+    var currentKeyWindow: UIWindow? {
         if #available(iOS 15.0, *) {
-            return self.connectedScenes.filter { $0.activationState == .foregroundActive }
-                // Keep only active scenes, onscreen and visible to the user
-                .filter { $0.activationState == .foregroundActive }
-                // Keep only the first `UIWindowScene`
+            return self.connectedScenes
+                .filter { $0.activationState == .foregroundActive || $0.activationState == .foregroundInactive }
                 .first(where: { $0 is UIWindowScene })
-                // Get its associated windows
                 .flatMap({ $0 as? UIWindowScene })?.windows
-                // Finally, keep only the key window
-                .first(where: \.isKeyWindow)
+                .first{ $0.isKeyWindow }
         } else {
             return self.windows.filter {$0.isKeyWindow}.first
         }
