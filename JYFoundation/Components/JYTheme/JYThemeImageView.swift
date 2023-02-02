bc120b2c163336b6c86a999d08bd87bc4631fa1d
scott.rong <sunshine121981@hotmail.com>
2023-02-02 12:41:27 +0800

    add jythemeiamgeview

---
+++ JYFoundation/Components/JYTheme/JYThemeImageView.swift
@@ -0,0 +1,54 @@
+//
+//  JYThemeImageView.swift
+//  JYFoundation
+//
+//  Created by 荣超 on 2023/2/2.
+//  Copyright © 2023 jayasme. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+open class JYThemeImageView: UIImageView, JYThemeful {
+    
+    public var themes: [JYTheme] = [] {
+        didSet {
+            self.applyTheme()
+        }
+    }
+    
+    public var styleSheet: JYStyleSheet? {
+        didSet {
+            self.applyTheme()
+        }
+    }
+    
+    public override var image: UIImage? {
+        get {
+            return super.image
+        }
+        set {
+            super.image = self.getStyledImage(image: newValue)
+        }
+    }
+    
+    func applyTheme() {
+        self.backgroundColor = self.styleSheet?.backgroundColor?.style(by: self.themes).first ?? .clear
+        self.image = self.getStyledImage(image: self.image)
+    }
+    
+    func getStyledImage(image: UIImage?) -> UIImage? {
+        var image = image
+        guard image != nil else {
+            return nil
+        }
+        if #available(iOS 13.0, *), let foregroundColor = self.styleSheet?.foregroundColor?.style(by: self.themes).first {
+            image = image!.withTintColor(foregroundColor, renderingMode: .alwaysOriginal)
+        }
+        if let font = self.styleSheet?.font?.style(by: self.themes).first {
+            image = image!.jy_resize(with: CGSize(width: font.pointSize, height: font.pointSize))
+        }
+        return image
+    }
+}
